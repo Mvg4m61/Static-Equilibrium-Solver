8c0d988e845c7c905b359131041d0cd8e14c2454
Hiram <116443893+Mvg4m61@users.noreply.github.com>
2023-10-14 21:48:48 +0300

    Update in_static_equilibrium.py

--- in_static_equilibrium.py
+++ in_static_equilibrium.py
@@ -50,6 +50,32 @@ def in_static_equilibrium(
     False
     """
     # summation of moments is zero
+
+    
     moments: NDArray[float64] = cross(location, forces)
     sum_moments: float = sum(moments)
     return abs(sum_moments) < eps
+    
+
+if __name__ == "__main__":
+    # Test to check if it works
+    forces = array(
+        [
+            polar_force(718.4, 180 - 30),
+            polar_force(879.54, 45),
+            polar_force(100, -90),
+        ]
+    )
+
+    location: NDArray[float64] = array([[0, 0], [0, 0], [0, 0]])
+
+    assert in_static_equilibrium(forces, location)
+
+    # Problem 1 in image_data/2D_problems.jpg
+    forces = array(
+        [
+            polar_force(30 * 9.81, 15),
+            polar_force(215, 180 - 45),
+            polar_force(264, 90 - 30),
+        ]
+    )
